6463d1b92fa0a04c2f05ccca0e96ee04ab0ef3ca
leedonghu <ldh930517@gmail.com>
2021-06-21 12:41:45 +0900

    6/21

---
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -0,0 +1,31 @@
+package org.zerock.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Select;
+import org.zerock.domain.BoardVO;
+
+public interface BoardMapper {
+	
+	//@Select("SELECT * FROM tbl_board")
+	public List<BoardVO> getList();
+	
+	
+	//INSERT INTO tbl_board (title, content, writer) 
+	//VALUES (#{title}, #{content}, #{writer} )
+	public int insert(BoardVO board);
+	
+	/*
+	 String sql = "INSERT INTO tbl_board (title, content, writer) VALUES (?,?,?) ";
+	 ...
+	 pstmt.setString(1, board.getTitle());
+	 ...
+	 pstmt.executeUpdate(); 
+	 
+	 */
+	
+	public int insertSelectKey(BoardVO board);
+	
+	//하나의 record만 얻어오는 method
+	public BoardVO read(long bno);
+}
---
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -0,0 +1,98 @@
+package org.zerock.mapper;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	
+	@Test
+	public void testGetList() {
+		assertNotNull(mapper); // mapper가 있는지 확인
+		
+		List<BoardVO> list = mapper.getList();
+		
+		//assertEquals(5, list.size()); //table의 크기가 5인지 확인
+		                                //root-context.xml에서 스키마 확인(spr1)
+		assertTrue(list.size() >0);
+	}
+	
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+		
+		int cnt = mapper.insert(board);
+		
+		assertEquals(1, cnt);
+		
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		BoardVO board = new BoardVO();
+		board.setTitle("asd");
+		board.setContent("cdftt");
+		board.setWriter("donghu");
+		
+		assertEquals(0, board.getBno());
+		
+		int cnt = mapper.insertSelectKey(board);
+		
+		assertEquals(1, cnt);
+		assertNotEquals(0, board.getBno());
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO vo =  mapper.read(1);
+		
+		assertNotNull(vo);
+		assertEquals(1, vo.getBno());
+		
+		/* INSERT, 자동 증가 키값 확인*/
+		BoardVO board = new BoardVO();
+		board.setTitle("asd");
+		board.setContent("cdftt");
+		board.setWriter("donghu");
+		
+		assertEquals(0, board.getBno());
+		
+		int cnt = mapper.insertSelectKey(board);
+		
+		long key = board.getBno();
+		
+		BoardVO newBoard = mapper.read(key);
+		
+		assertNotNull(newBoard);
+		assertEquals(key, newBoard.getBno());
+		
+		
+		
+		
+	}
+
+}
