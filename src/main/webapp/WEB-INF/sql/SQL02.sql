fa73003261d2ac6b194a83977deb8ae1e9a87bb3
leedonghu <ldh930517@gmail.com>
2021-06-24 15:37:53 +0900

    6/24

---
+++ src/main/webapp/WEB-INF/sql/SQL02.sql
@@ -0,0 +1,11 @@
+SELECT * FROM tbl_board
+order BY bno DESC
+LIMIT 0, 5;
+
+SELECT * FROM tbl_board
+order BY bno DESC
+LIMIT 5, 5;
+
+SELECT * FROM tbl_board
+order BY bno DESC
+LIMIT 10, 5; -- n 번째 페이지 limit  5 * (n-1) , 5
